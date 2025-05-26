16df5b4dbfbb5f922ec2c266610f47b71c25409c
Rhisland OGAGNA <rhisland@hotmail.fr>
2025-05-26 21:58:19 +0200

    Ajout mise à jour sauvegarde.sh

---
+++ sauvegarde.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e           # Stop le script si une commande échoue
+set -u           # Stop si une variable non initialisée est utilisée
+set -o pipefail  # Stop si une commande dans un pipeline échoue
+
+DOSSIER="./rapports/"
+
+for fichier in "$DOSSIER"/*; do
+   if [[ "$fichier" == *.bak ]]; then
+     echo "$fichier - Est un fichier de sauvegarde, arrêt du scan"
+     break
+         elif [[ "$fichier" == *.txt ]]; then
+           mkdir -p ./sauvegardes
+           timestamp=$(date)
+           cp -p "$fichier" ./sauvegardes/"$(basename $fichier)_sauve_${timestamp}.txt"
+          echo "sauvegardes/$(basename $fichier)_sauve_${timestamp}.txt" >> sauvegarde.log
+         else
+          echo "$fichier :" "Non pris en compte par le script"
+   fi
+done
